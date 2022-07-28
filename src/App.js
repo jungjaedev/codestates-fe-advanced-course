4634fd0f6025bcb178f55f08d23bd1e0f44f3c78
Jaewon <jungjaedev@gmail.com>
2022-07-28 23:26:46 +0900

    Add : get data by using axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <div className="App"></div>;
+  const [posts, setPosts] = useState();
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(data => {
+      console.log(data);
+      setPosts(data.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      {posts.map((el, idx) => {
+        console.log(el);
+        return <div key={idx}>{el.title}</div>;
+      })}
+    </div>
+  );
 }
 
 export default App;
